d86d01aa34dc39b78f78cdabe2b99e2738018368
ASURA103 <shivrahulrajput@gmail.com>
2026-06-05 20:46:17 +0530

    Initial project structure

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    < div className=" ">
+    <div   className="  text-5xl text-black font-bold  justify-self-center">App</div>
+    </div>
+  )
+}
+
+export default App
